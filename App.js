4053b5ed348219ab95e698bf3ee1e10e83fe2433
parkjigyu <qkr13wlgus57@craftory.info>
2020-04-08 00:40:08 +0900

    Styling the To Do Component

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions,Platform } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions,Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo";
 const { height, width } = Dimensions.get("window");
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Kawai To Do</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}/>
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
+  render(){
+    const {newToDo} = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    )
+  }
+  _controlNewToDo = text =>{
+    this.setState({
+      newToDo : text
+    })
+  }
 }
 
 const styles = StyleSheet.create({
@@ -47,5 +68,14 @@ const styles = StyleSheet.create({
         elevation : 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    paddingLeft: 10
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react"
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
+
+const { width, height } = Dimensions.get("window");
+export default class ToDo extends Component{
+    state = {
+        isEditing: false,
+        isCompleted: false
+    }
+    render(){
+        const {isCompleted,isEditing} = this.state;
+        return(
+            <View style={styles.container}>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View 
+                            style={[
+                                styles.circle, isCompleted ? styles.completeCircle : styles.unCompleteCircle
+                            ]}
+                        />
+                    </TouchableOpacity>
+                    <Text style={[
+                        styles.text,isCompleted ? styles.completeText : styles.unCompleteText
+                        ]}
+                    >
+                        Hello I'm a To Do
+                    </Text>
+                </View>
+
+                {isEditing ? (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._finishEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>✅</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._startEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>✏️</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>❌</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </View>
+        );
+    }
+    _toggleComplete = () => {
+        this.setState(prevState => {
+            return {
+                isCompleted: !prevState.isCompleted
+            }
+        })
+    };
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        })
+    };
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
+        })
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor:"#bbb",
+        borderBottomWidth:StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    circle: {
+        width:30,
+        height:30,
+        borderRadius:15,
+        borderWidth:3,
+        marginRight: 20
+    },
+    completeCircle: {
+        borderColor:"#bbb"
+    },
+    unCompleteCircle: {
+        borderColor:"#F23657"
+    },
+    text: {
+        fontWeight: "600",
+        fontSize: 20,
+        marginVertical: 20
+    },
+    completeText: {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    unCompleteText: {
+        color: "#353839"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: width / 2,
+        justifyContent: "space-between"
+    },
+    actions: {
+        flexDirection: "row",
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
+    }
+})
